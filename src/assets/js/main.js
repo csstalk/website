2d75cd18cef8474a0c29b994f4ba42cf136a2b2b
matsuo@TAM <matsuo@tam-tam.co.jp>
2017-03-19 23:17:34 +0900

    トップ Hero と About を作業

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,33 +2,10 @@
 
 window.jQuery = window.$ = require('jquery');
 
-import ResizeManager from './util/ResizeManager';
-import ScrollManager from './util/ScrollManager';
-
-import StructureModule1 from '../namespace/TopPage/Module1/_Module1.js';
-
-const resizeManager = new ResizeManager();
-const scrollManager = new ScrollManager();
-
-export const mainResizeManager = resizeManager;
+import DesignView from './util/DesignView';
 
 (() => {
   $(() => {
-    console.log('page loaded');
-
-    StructureModule1();
-
-    resizeManager.add(resized01);
-    resizeManager.init();
-
-    scrollManager.add(scroll01);
-    scrollManager.init();
+    DesignView();
   });
-
-  const resized01 = () => {
-    console.log('is resized! 01');
-  };
-  const scroll01 = () => {
-    console.log('is scrolled! 01');
-  };
 })();
