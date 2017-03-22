cbc841d86468001f65c3c011e0dd1e857321e82b
matsuo@TAM <matsuo@tam-tam.co.jp>
2017-03-22 21:10:36 +0900

    マイクのアニメーション (暫定)

---
+++ src/assets/js/Home/Mic.js
@@ -0,0 +1,15 @@
+'use strict';
+
+export default () => {
+
+  const mic_offset_top = $('.home-Mic').offset().top + 300;
+
+  $(window).on({
+    'scroll': () => {
+      if ($(window).scrollTop() + $(window).height() > mic_offset_top) {
+        $('.home-Mics').addClass('home-Mic_Animate');
+        $(window).off('scroll');
+      }
+    }
+  });
+}
